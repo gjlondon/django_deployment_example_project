1b0901ca93d6a0b140e98644cf3dd4d5e49a3b07
rogueleaderr <4.business.email@gmail.com>
2013-10-09 15:10:45 -0400

    removed project.settings dependency

--- deployment_example_project/settings/__init__.py
+++ deployment_example_project/settings/__init__.py
@@ -1,13 +1,18 @@
 import os
 import sys
 
-from .deployment_example_project.deployment_example_project.settings.base import *
-
-execfile("../../project.settings")
-name = os.getenv("PROJECT_NAME")
+# application_python cookbook expects manage.py in a top level
+# instead of app level dir, so the relative import can fail
+try:
+    from .deployment_example_project.deployment_example_project.settings.base import *
+except ImportError:
+    from deployment_example_project.settings.base import *
 
 try:
-    sys.path.append("/srv/{name}/shared".format(name=name))
+    # add the /shared directory so that migrate can run
+    # before the /release directories are symlinked
+    sys.path.append(sys.path[0].rsplit("/", 1)[0])
     from local_settings import *
 except ImportError:
+    print "import error"
     pass
