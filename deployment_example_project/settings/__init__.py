1d43e6edcca85d5729facdfcbbd06d9eb481da63
rogueleaderr <4.business.email@gmail.com>
2013-10-14 15:36:20 -0400

    sys path correction

--- deployment_example_project/settings/__init__.py
+++ deployment_example_project/settings/__init__.py
@@ -8,13 +8,12 @@ try:
 except ImportError:
     from deployment_example_project.settings.base import *
 
-"""
+
 try:
     # add the /shared directory so that migrate can run
     # before the /release directories are symlinked
-    sys.path.append(sys.path[0].rsplit("/", 1)[0])
+    #sys.path.append(sys.path[0].rsplit("/", 1)[0])
     from local_settings import *
 except ImportError:
     print "import error"
     pass
-"""
